84a750adb125dfee22895d0220fe67b45bc734da
Maksar <adminochka@gmail.com>
2022-02-14 00:31:56 +0200

    Updates

---
+++ precache-manifest.4f056b36b871a6e3d4d758ea1e7cc725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5119679ef056bc61197b266ebbb05e25",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "6a44fffe116f9c860fe9",
+    "url": "/todo-react/static/css/main.f4903a8f.chunk.css"
+  },
+  {
+    "revision": "f09b246faf15a1ff0037",
+    "url": "/todo-react/static/js/2.85a581b9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-react/static/js/2.85a581b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a44fffe116f9c860fe9",
+    "url": "/todo-react/static/js/main.6e502a7b.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
